2d35dc36d310c4619789e8114b410fe17033319d
Der.L <sleechengn@hotmail.com>
2025-02-28 14:25:51 +0800

    update

--- build-local.sh
+++ build-local.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-docker build $(dirname $0) --file Dockerfile -t 192.168.13.73:5000/sleechengn/f5-tts:latest
+bash build.sh 192.168.13.73:5000/sleechengn/f5-tts:latest
 docker push 192.168.13.73:5000/sleechengn/f5-tts:latest
--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
-
-docker build $(dirname $0) --file Dockerfile -t sleechengn/f5-tts:latest
+if [ -n "$1" ]; then
+	docker build $(dirname $0) --file Dockerfile -t $1
+else
+	docker build $(dirname $0) --file Dockerfile -t sleechengn/f5-tts:latest
+fi
